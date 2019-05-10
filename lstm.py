3852e3dd583b0fe5b4432faf5150a74224308311
burnmg <rlin1992@hotmail.com>
2019-05-10 18:30:28 -0400

    change model

--- lstm.py
+++ lstm.py
@@ -95,12 +95,14 @@ def create_network(network_input, n_vocab):
     model.add(Dropout(0.3))
     model.add(LSTM(512, return_sequences=True))
     model.add(Dropout(0.3))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(Dropout(0.3))
     model.add(LSTM(512))
     model.add(Dense(256))
     model.add(Dropout(0.3))
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
 
     return model
 
