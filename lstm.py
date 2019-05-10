b7e6e9210c8909bdc61f8c66413f8dda0993db1d
burnmg <rlin1992@hotmail.com>
2019-05-10 18:38:52 -0400

    change model

--- lstm.py
+++ lstm.py
@@ -120,5 +120,15 @@ def train(model, network_input, network_output):
 
     model.fit(network_input, network_output, epochs=200, batch_size=64, callbacks=callbacks_list)
 
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+
+
+
 if __name__ == '__main__':
     train_network()
